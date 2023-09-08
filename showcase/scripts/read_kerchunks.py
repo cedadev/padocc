7cc55f710ad0441423b9752a731303c1e97cddbd
dwest77 <daniel.westwood@stfc.ac.uk>
2023-09-08 16:10:51 +0100

    Added read_kerchunk script

---
+++ showcase/scripts/read_kerchunks.py
@@ -0,0 +1,42 @@
+from kerchunk import hdf, combine, df
+import fsspec.implementations.reference
+from fsspec.implementations.reference import LazyReferenceMapper
+from tempfile import TemporaryDirectory
+
+import matplotlib.pyplot as plt
+
+import json
+
+import xarray as xr
+import os, sys
+from datetime import datetime
+
+PATH = '/home/users/dwest77/Documents/kerchunk_dev/parquet/dev/'
+pq = f'{PATH}/batch/esacci_7'
+
+
+parquet = '-p' in sys.argv
+
+test_json = '/gws/nopw/j04/esacci_portal/kerchunk/kfiles/esacci4.json'
+#@profile
+def main():
+    if parquet:
+        t1 = datetime.now()
+        fs = fsspec.implementations.reference.ReferenceFileSystem(
+            pq, remote_protocol='file', target_protocol="file", lazy=True)
+        ds = xr.open_dataset(
+            fs.get_mapper(), engine="zarr",
+            backend_kwargs={"consolidated": False, "decode_times": False}
+        )
+        ds['chlor_a'][0,2059:2284, 4219:4444].plot()
+        plt.savefig('img/largeparq.png')
+        print(parquet, (datetime.now()-t1).total_seconds())
+    else:
+        t1 = datetime.now()
+        mapper = fsspec.get_mapper('reference://', fo='cache')
+        ds = xr.open_zarr(mapper)
+        ds['chlor_a'][:,2059:2284, 4219:4444].mean(dim='time').plot()
+        plt.savefig('img/kerc.png')
+        print(parquet, (datetime.now()-t1).total_seconds())
+main()
+#print(ds['var1'])
