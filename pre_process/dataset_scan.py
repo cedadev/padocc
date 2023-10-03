b91e48749d3bcc50d4586b9af1382913bbd30086
dwest77 <daniel.westwood@stfc.ac.uk>
2023-10-03 16:32:04 +0100

    Updated scanner

--- pre_process/dataset_scan.py
+++ pre_process/dataset_scan.py
@@ -17,23 +17,23 @@ import numpy as np
 
 # Quick open a netcdf file
 
-VERBOSE = '-v' in sys.argv
+VERBOSE = True
 
 def vprint(msg):
     if VERBOSE:
-        vprint(msg)
+        print(msg)
 
 def count_refs(nfile):
     vprint(nfile)
     try:
         vprint('Using HDF5 reader')
         tdict = SingleHdf5ToZarr(nfile, inline_threshold=1).translate()
-        return len(json.dumps(tdict['refs']))/1000, len(tdict['refs'].keys())
+        return tdict['refs']
     except OSError:
         vprint('Switching to NetCDF3 reader')
         try:
             tdict = NetCDF3ToZarr(nfile, inline_threshold=1).translate()
-            return len(json.dumps(tdict['refs']))/1000, len(tdict['refs'].keys())
+            return tdict['refs']
         except:
             return False
         return False
@@ -41,9 +41,8 @@ def count_refs(nfile):
 def get_base_size(nfile):
     return os.stat(nfile).st_size
 
-def recursive_findfiles(path,d, verbose=False):
-    if verbose:
-        vprint('Recursive file search level:',d)
+def recursive_findfiles(path,d):
+    vprint('Recursive file search level:',d)
     nfiles = []
     files = os.listdir(path)
     for f in files:
@@ -53,8 +52,7 @@ def recursive_findfiles(path,d, verbose=False):
         elif os.path.isdir(fpath):
             nfiles = nfiles + recursive_findfiles(fpath,d + 1, verbose=verbose)
         else:
-            if verbose:
-                vprint('Ignoring path',fpath)
+            vprint('Ignoring path',fpath)
     return nfiles
 
 def getsizes(nfiles, path):
@@ -67,96 +65,87 @@ def get_internals(path, nfiles):
     inner = False
     counter = 0
     while not inner:
-        inner, refs = count_refs(os.path.join(path, nfiles[counter]))
-        if not inner:
-            vprint('Kerchunking failed on file', counter)
-        counter += 1
+        try:
+            refs = count_refs(os.path.join(path, nfiles[counter]))
+            inner = True
+        except:
+            pass
         if counter > 100:
             inner = True
     if counter > 100:
         vprint('Kerchunk Aborted - Files not kerchunkable')
         return None
-    return inner, refs
+    sizes = []
+    chunks = 0
+    for chunkkey in refs.keys():
+        try:
+            sizes.append(int(refs[chunkkey][2]))
+            chunks += 1
+        except:
+            pass
+    return sizes, chunks
 
 
-def scan_path(path, verbose, base_eval=False):
+def scan_path(path, base_eval=False):
     if '*' in path:
         nfiles = sorted(glob.glob(path))
     else:
         if os.path.isfile(path):
             vprint('PathError: Please put single quotes around patterns with "*" wildcards')
             return None
-        nfiles = recursive_findfiles(path, 1, verbose=verbose)
+        nfiles = recursive_findfiles(path, 1)
 
     if not len(nfiles) > 0:
         vprint('FileError: .nc file count is zero for ',path)
         return None
 
-    bytetotal, refs = get_internals(path, nfiles)
-    
+    sizes, chunks_per_file = get_internals(path, nfiles)
     filecount = len(nfiles)
-    nsizes    = getsizes(nfiles, path)
-    refcount  = int(refs)*int(filecount) # Estimate total refs
-    msize     = int(bytetotal)*int(filecount)*(np.mean(nsizes)/nsizes[0])/1000 # Memory in MB
+    total_chunk_size = np.sum(sizes)
 
-    # Calculate errors
-    err = np.std(nsizes)/(math.sqrt(len(nsizes))*np.mean(nsizes))
+    return chunks_per_file, filecount, total_chunk_size
 
-    if msize < 1:
-        krating = 'Good'
-    elif msize >= 1 and msize < 100:
-        krating = 'Average'
-    else:
-        krating = 'Poor'
-    bytecount = f'{bytetotal*1000/refs:.3f}'
-    memsize   = f'{msize:.3f}'
 
-    vprint(ASSESS.format(filecount, refs, bytecount, refcount, krating, memsize))
-
-    if base_eval:
-        meansize = f'{np.mean(sizes)/1000000:.3f}'
-        maxsize  = f'{np.max(sizes)/1000000:.3f}'
-        total    = f'{np.sum(sizes)/1000000000:.3f}'
-
-        vprint(BASE.format(meansize, maxsize, total))
-
-    multiplier = np.mean(nsizes)/nsizes[0]
-    return filecount, multiplier, err
-    # 30,000,000 ~ 5000 MB
-    # Suitability - Good (<1MB), Medium (1MB-100MB), Poor(>100MB)
-
-# Assume deal with the first file in a directory
-
-directory = sys.argv[1] #'/badc/cmip6/data/CMIP6/ScenarioMIP/NCC//NorESM2-MM/ssp585/r1i1p1f1/Amon/va/gn/latest/'
-nickname = None
-if '-n' in sys.argv:
-    for x in range(len(sys.argv)):
-        if sys.argv[x] == '-n':
-            nickname = sys.argv[x+1]
-
-base_eval = '-b' in sys.argv
-
-raw = '-r' in sys.argv
-
-def main(directory):
-    vprint('Assessment for ', directory)
-    chunks_per_file, numfiles, total_ = scan_path(directory, verbose, base_eval=base_eval)
+def main(directory, outdir, nickname):
+    vprint('Assessment for '+directory)
+    chunks_per_file, numfiles, total_chunk_size = scan_path(directory, base_eval=base_eval)
     if numfiles:
 
         if nickname:
             dsname = nickname
         else:
             dsname = '-'.join(directory.split('/')[1:3])
-        
-        with open(f'{dsname}-cfg.txt') as f:
+        with open(f'{outdir}/configs/{dsname}-cfg.txt','w') as f:
             f.write(f'{chunks_per_file},{numfiles},{total_chunk_size}')
+        vprint(f'Written config info to {dsname}-cfg.txt')
 
-if '-f' in sys.argv:
-    vprint('[INFO] Identifying file containing patterns')
-    f = open(directory,'r')
-    dirs = [q.replace('\n','') for q in f.readlines()]
-    f.close()
-    for d in dirs:
-        main(d)
-else:
-    main(directory)
+# Assume deal with the first file in a directory
+if __name__ == '__main__':
+    kstore_name = sys.argv[-1]
+    pattern = sys.argv[-2] #'/badc/cmip6/data/CMIP6/ScenarioMIP/NCC//NorESM2-MM/ssp585/r1i1p1f1/Amon/va/gn/latest/'
+    try:
+        outdir = os.environ['OUTDIR']
+    except KeyError:
+        outdir = '/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder/test_parqs'
+
+    os.system(f'ls {pattern} > {outdir}/filelists/{kstore_name}.txt')
+
+    nickname = None
+    if '-n' in sys.argv:
+        for x in range(len(sys.argv)):
+            if sys.argv[x] == '-n':
+                nickname = sys.argv[x+1]
+
+    base_eval = '-b' in sys.argv
+
+    raw = '-r' in sys.argv
+
+    if '-f' in sys.argv:
+        vprint('[INFO] Identifying file containing patterns')
+        f = open(pattern,'r')
+        dirs = [q.replace('\n','') for q in f.readlines()]
+        f.close()
+        for d in dirs:
+            main(d, outdir, kstore_name)
+    else:
+        main(pattern, outdir, kstore_name)
