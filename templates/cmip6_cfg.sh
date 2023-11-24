80983d3ea6a504a23c78b2fde86c729f50b84b16
dwest77 <daniel.westwood@stfc.ac.uk>
2023-11-24 15:11:22 +0000

    Added env variables and changed argument options

--- templates/cmip6_cfg.sh
+++ templates/cmip6_cfg.sh
@@ -1,2 +1,4 @@
 export WORKDIR=/gws/nopw/j04/cmip6_prep_vol1/kerchunk-pipeline/in_progress
 export GROUPDIR=/gws/nopw/j04/cmip6_prep_vol1/kerchunk-pipeline/groups
+export SRCDIR=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder
+export KVENV=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder/build_venv
