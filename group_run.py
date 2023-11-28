63da686225e32c3f0718b4affb393d3d09f38cc6
dwest77 <daniel.westwood@stfc.ac.uk>
2023-11-28 16:53:17 +0000

    Updated flag options

--- group_run.py
+++ group_run.py
@@ -3,17 +3,29 @@ import json
 import os
 import argparse
 
-def get_group_len(groupdir, group):
+def output(msg,verb=True, mode=None, log=None, pref=0):
+    prefixes = ['INFO','ERR']
+    prefix = prefixes[pref]
+    if verb:
+        if mode == 'log':
+            log += f'{prefix}: {msg}\n'
+        else:
+            print(f'> {prefix}: {msg}')
+    return log
+
+def get_group_len(workdir, group):
     # Implement parallel reads from single 'group' file
-    with open(f'{groupdir}/filelists/{group}.txt') as f:
+    with open(f'{workdir}/groups/filelists/{group}.txt') as f:
         group_len = len(list(f.readlines()))
     return group_len
 
 times = {
     'scan':'5:00',
-    'compute':'',
+    'compute':'20:00',
     'test':''
 }
+
+phases = list(times.keys())
 
 def get_attribute(env, args, var):
     if os.getenv(env):
@@ -31,21 +43,30 @@ def main(args):
     group   = args.group
 
     WORKDIR  = get_attribute('WORKDIR', args, 'workdir')
+    if not WORKDIR:
+        output('WORKDIR missing or undefined', verb=args.verbose)
+        return None
     SRCDIR   = get_attribute('SRCDIR', args, 'source')
+    if not SRCDIR:
+        output('SRCDIR missing or undefined', verb=args.verbose)
+        return None
     VENV     = get_attribute('KVENV', args, 'kvenv')
+    if not VENV:
+        output('VENV missing or undefined', verb=args.verbose)
+        return None
     GROUPDIR = f'{WORKDIR}/groups/{group}'
 
     # Establish some group parameters
-    group_len          = get_group_len(GROUPDIR, group)
-    group_phase_sbatch = f'{GROUPDIR}/{group}/sbatch/{phase}.sbatch'
+    group_len          = get_group_len(WORKDIR, group)
+    group_phase_sbatch = f'{GROUPDIR}/sbatch/{phase}.sbatch'
     master_script      = f'{SRCDIR}/single_run.py'
     template           = 'templates/phase.sbatch.template'
 
 
     # Make Directories
     for dirx in ['sbatch','outs','errs']:
-        if not os.path.isdir(f'{GROUPDIR}/{group}/{dirx}'):
-            os.makedirs(f'{GROUPDIR}/{group}/{dirx}')
+        if not os.path.isdir(f'{GROUPDIR}/{dirx}'):
+            os.makedirs(f'{GROUPDIR}/{dirx}')
 
     if phase not in phases:
         print(f'Error: "{phase}" not recognised, please select from {phases}')
@@ -53,15 +74,22 @@ def main(args):
 
     with open(template) as f:
         sbatch = '\n'.join([r.strip() for r in f.readlines()])
+
     sb = sbatch.format(
-        f'{group}_{phase}_array',
-        times[phase],
-        f'{GROUPDIR}/{group}/outs/%A_%a.out',
-        f'{GROUPDIR}/{group}/errs/%A_%a.err',
+        f'{group}_{phase}_array',             # Job name
+        times[phase],                         # Time
+        f'{GROUPDIR}/outs/%A/%a.out', # Outs
+        f'{GROUPDIR}/errs/%A/%a.err', # Errs
         VENV,
-        master_script,
-        group
+        WORKDIR,
+        GROUPDIR,
+        master_script, phase, group
     )
+    if args.forceful:
+        sb += ' -f'
+    if args.verbose:
+        sb += ' -v'
+
     with open(group_phase_sbatch,'w') as f:
         f.write(sb)
 
@@ -73,8 +101,11 @@ if __name__ == '__main__':
     parser.add_argument('phase', type=str, help='Phase of the pipeline to initiate')
     parser.add_argument('group',type=str, help='Group identifier code')
     parser.add_argument('-w',dest='workdir', help='Working directory for pipeline')
+    parser.add_argument('-g',dest='groupdir', help='Group directory for pipeline')
     parser.add_argument('-s',dest='source', help='Path to directory containing master scripts (this one)')
     parser.add_argument('-e',dest='venvpath', help='Path to virtual (e)nvironment (excludes /bin/activate)')
+    parser.add_argument('-f',dest='forceful', action='store_true', help='Force overwrite of steps if previously done')
+    parser.add_argument('-v',dest='verbose' , action='store_true', help='Print helpful statements while running')
     args = parser.parse_args()
 
     main(args)
--- single_run.py
+++ single_run.py
@@ -6,6 +6,17 @@ __copyright__ = "Copyright 2023 United Kingdom Research and Innovation"
 import sys
 import argparse
 import os
+import json
+
+def output(msg,verb=True, mode=None, log=None, pref=0):
+    prefixes = ['INFO','ERR']
+    prefix = prefixes[pref]
+    if verb:
+        if mode == 'log':
+            log += f'{prefix}: {msg}\n'
+        else:
+            print(f'> {prefix}: {msg}')
+    return log
 
 def run_init(args):
     from scripts.init import init_config
@@ -14,18 +25,89 @@ def run_init(args):
 def run_scan():
     pass
 
+def run_compute(args):
+    from scripts.compute.serial_process import Indexer
+
+    log = output(f'Starting computation step for {args.proj_code}', verb=args.verbose, mode=args.mode,log=None)
+
+    cfg_file = f'{args.proj_dir}/base-cfg.json'
+    detail_file = f'{args.proj_dir}/detail-cfg.json'
+
+    if not os.path.isfile(cfg_file):
+        print(f'Error: cfg file missing or not provided - {cfg_file}')
+        return None
+    
+    if not os.path.isfile(detail_file):
+        print(f'Error: cfg file missing or not provided - {detail_file}')
+        return None
+    
+    version_no = 1
+    complete, escape = False, False
+    while not (complete or escape):
+        out_json = f'{args.proj_dir}/kerchunk-{version_no}a.json'
+        out_parq = f'{args.proj_dir}/kerchunk-{version_no}a.parq'
+
+        if os.path.isfile(out_json) or os.path.isfile(out_parq):
+            if args.forceful:
+                complete = True
+            elif args.new_version:
+                version_no += 1
+            else:
+                escape = True
+        else:
+            complete = True
+
+    concat_msg = '' # CMIP and CCI may be different?
+
+    if complete and not escape:
+
+        Indexer(args.proj_code, cfg_file=cfg_file, detail_file=detail_file, 
+                workdir=args.workdir, issave_meta=False, forceful=args.forceful,
+                verb=args.verbose, mode=args.mode,
+                version_no=version_no, concat_msg=concat_msg).create_refs()
+    else:
+        log = output('Output file already exists and there is no plan to overwrite', mode=args.mode,log=None, pref=1)
+        return None
+
+
 drivers = {
     'init':run_init,
-    'scan':run_scan
+    'scan':run_scan,
+    'compute': run_compute
 }
 
+def get_proj_code(groupdir, pid, groupid):
+    with open(f'{groupdir}/proj_codes.txt') as f:
+        proj_code = f.readlines()[int(pid)].strip()
+    return proj_code
+
+def get_attribute(env, args, var):
+    if os.getenv(env):
+        return os.getenv(env)
+    elif hasattr(args, var):
+        return getattr(args, var)
+    else:
+        print(f'Error: Missing attribute {var}')
+        return None
+    
+
 def main(args):
-    if os.getenv('WORKDIR'):
-        args.workdir = os.getenv('WORKDIR')
+    args.workdir  = get_attribute('WORKDIR', args, 'workdir')
+    args.groupdir = get_attribute('GROUPDIR', args, 'groupdir')
 
     if not args.workdir:
         print('Error: No working directory given as input or from environment')
         return None
+    
+    if args.groupID:
+        args.proj_code = get_proj_code(args.groupdir, args.proj_code, args.groupID)
+        if not args.proj_dir:
+            # Load in all base config settings
+            args.proj_dir = f'{args.workdir}/in_progress/{args.groupID}/{args.proj_code}'
+
+    if not args.proj_dir:
+        # Load in all base config settings
+        args.proj_dir = f'{args.workdir}/in_progress/{args.proj_code}'
 
     # Run the specified phase
     if args.phase in drivers:
@@ -35,10 +117,18 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run a pipeline step for a single dataset')
-    parser.add_argument('phase', type=str, help='Phase of the pipeline to initiate')
+    parser.add_argument('phase', type=str,    help='Phase of the pipeline to initiate')
     parser.add_argument('proj_code',type=str, help='Project identifier code')
-    parser.add_argument('-w',dest='workdir', help='Working directory for pipeline')
-    parser.add_argument('-g',dest='groupID', help='Group identifier label')
+    parser.add_argument('-w',dest='workdir',  help='Working directory for pipeline')
+    parser.add_argument('-g',dest='groupdir', help='Group directory for pipeline')
+    parser.add_argument('-G',dest='groupID',  help='Group identifier label')
+    parser.add_argument('-p',dest='proj_dir', help='Project directory for pipeline')
+    parser.add_argument('-f',dest='forceful', action='store_true', help='Force overwrite of steps if previously done')
+    parser.add_argument('-n',dest='new_version', help='If present, create a new version')
+    parser.add_argument('-v',dest='verbose' , action='store_true', help='Print helpful statements while running')
+    parser.add_argument('-m',dest='mode'    , help='Print or record information (log or std)')
+
+
     args = parser.parse_args()
     main(args)
 
