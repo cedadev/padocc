bdf79968433a11a496c5bfb0ade97fc3699a72e3
dwest77 <daniel.westwood@stfc.ac.uk>
2024-01-25 14:19:34 +0000

    Testing summary 2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,8 +22,6 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-theme='jekyll-theme-minimal'
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
