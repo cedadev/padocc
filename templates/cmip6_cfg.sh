c05a10b071a36e606f8e7fd798a88f2163f475b4
dwest77 <daniel.westwood@stfc.ac.uk>
2024-01-25 10:33:49 +0000

    Changed filenames

--- templates/cmip6_cfg.sh
+++
@@ -1,4 +0,0 @@
-export WORKDIR=/gws/nopw/j04/cmip6_prep_vol1/kerchunk-pipeline
-export GROUPDIR=/gws/nopw/j04/cmip6_prep_vol1/kerchunk-pipeline/groups/CMIP6_rel1_6233
-export SRCDIR=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder
-export KVENV=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder/build_venv
