7fc1387add71d89a970e72d5ba8015fb32591c5d
dwest77 <daniel.westwood@stfc.ac.uk>
2024-03-06 12:34:24 +0000

    Moved scripts

--- extensions/templates/setup-cci.sh
+++
@@ -1,7 +0,0 @@
-module load jaspy
-
-export WORKDIR=/gws/nopw/j04/esacci_portal/kerchunk_conversion/
-export SRCDIR=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder
-export KVENV=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder/build_venv
-
-source $KVENV/bin/activate
--- extensions/templates/setup-metoff.sh
+++
@@ -1,3 +0,0 @@
-export WORKDIR=/home/users/dwest77/temp_netcdf/
-export SRCDIR=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder
-export KVENV=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder/build_venv
