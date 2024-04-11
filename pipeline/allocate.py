e7b0d56c6342fe786919af98a490856cced77fce
dwest77 <daniel.westwood@stfc.ac.uk>
2024-04-11 13:42:51 +0100

    Updated docstrings, comments

--- pipeline/allocate.py
+++ pipeline/allocate.py
@@ -12,15 +12,6 @@ __copyright__ = "Copyright 2023 United Kingdom Research and Innovation"
 # - End with N bins (subsets) - write list of project codes for each subset to a separate file in proj_code_subsets/set_N.txt
 # - Run array with number of subsets already set.
 
-# Utilisation estimate is (total_chunks * 835) + 500 (MB)
-"""
-for proj_code in (repeat_id set):
-    open detail-cfg (for this code)
-    calculate utilisation
-    add to dict [utilisation, proj_code]
-    keep track of max/min
-get bins using binpacking (pypi)
-"""
 import binpacking
 import os
 
@@ -42,29 +33,53 @@ def has_required_timings(detail) -> bool:
     return True
 
 def assemble_allocations(args):
+    """
+    Function for assembling all allocations and bands for packing. Allocations contain multiple processes within
+    a single SLURM job such that the estimates for time sum to less than the time allowed for that SLURM job. Bands
+    are single process per job, based on a default time plus any previous attempts (use --allow-band-increase flag
+    to enable band increases with successive attempts if previous jobs timed out)
+
+    :returns:   A list of tuple objects such that each tuple represents an array to submit to slurm with
+                the attributes (label, time, number_of_datasets). Note: The list of datasets to apply in
+                each array job is typcially saved under proj_codes/<repeat_id>/<label>.txt (allocations use
+                allocations/<x>.txt in place of the label)
+    """
     proj_codes = get_codes(args.groupID, args.workdir, f'proj_codes/{args.repeat_id}')
 
     time_estms = {}
-    time_defs_value = int(times[args.phase].split(':')[0])*2
+    time_defs_value = int(times[args.phase].split(':')[0])
     time_bands = {}
 
     for p in proj_codes:
         proj_dir       = get_proj_dir(p, args.workdir, args.groupID)
         detail         = get_proj_file(proj_dir, 'detail-cfg.json')
 
+        # Determine last run if present for this job
         lr = [None, None]
         if 'last_run' in detail:
             lr = detail['last_run']
         
-        # Experimental values for time estimation
         if has_required_timings(detail) and args.phase == 'compute':
-            time_estms[p] = 500 + (2.5 + 1.5*detail['timings']['convert_estm'])*detail['num_files']
+            # Calculate time estimation (minutes) - experimentally derived equation
+            time_estms[p] = (500 + (2.5 + 1.5*detail['timings']['convert_estm'])*detail['num_files'])/60 # Changed units to minutes for allocation
         else:
+            # Increase from previous job run if band increase allowed (previous jobs ran out of time)
             if lr[0] == args.phase and args.band_increase:
-                next_band = int(lr[1].split(':')[0]) + time_defs_value
+                try:
+                    next_band = int(lr[1].split(':')[0]) + time_defs_value
+                except:
+                    next_band = time_defs_value*2
             else:
+                # Use default if no prior info found.
                 next_band = time_defs_value
 
+            # Thorough/Quality validation - special case.
+            if 'quality_required' in detail and args.phase == 'validate':
+                if detail['quality_required']:
+                    # Hardcoded quality time 2 hours
+                    next_band = max(next_band, 120) # Min 2 hours
+
+            # Save code to specific band
             if next_band in time_bands:
                 time_bands[next_band].append(p)
             else:
@@ -74,6 +89,7 @@ def assemble_allocations(args):
         binsize = int(max(time_estms.values())*1.4/600)*600
         bins = binpacking.to_constant_volume(time_estms, binsize) # Rounded to 10 mins
     else:
+        # Unpack time_estms into established bands
         print('Skipped Job Allocations - using Bands-only.')
         bins = None
         for pc in time_estms.keys():
@@ -101,15 +117,21 @@ def assemble_allocations(args):
     # Create array bands
     create_array_bands(args.groupID, args.workdir, time_bands, args.repeat_id, dryrun=args.dryrun)
         
-    # Return a list of tuples: [ ('allocations', '240:00', 116), ...] 
-
     if len(time_bands) > 0:
         for b in time_bands:
             allocs.append((f"band_{b}", f'{b}:00', len(time_bands[b])))
 
+    # Return list of tuples.
     return allocs
 
-def create_allocations(groupID, workdir, bins, repeat_id, dryrun=False) -> None:
+def create_allocations(groupID: str, workdir: str, bins: list, repeat_id: str, dryrun=False) -> None:
+    """
+    Create allocation files (N project codes to each file) for later job runs.
+
+    :returns: None
+    """
+
+    # Ensure directory already exists.
     allocation_path = f'{workdir}/groups/{groupID}/proj_codes/{repeat_id}/allocations'
     if not os.path.isdir(allocation_path):
         if not dryrun:
@@ -120,6 +142,7 @@ def create_allocations(groupID, workdir, bins, repeat_id, dryrun=False) -> None:
     for idx, b in enumerate(bins):
         bset = b.keys()
         if not dryrun:
+            # Create a file for each allocation
             os.system(f'touch {allocation_path}/{idx}.txt')
             with open(f'{allocation_path}/{idx}.txt','w') as f:
                 f.write('\n'.join(bset))
@@ -127,13 +150,22 @@ def create_allocations(groupID, workdir, bins, repeat_id, dryrun=False) -> None:
             print(f'Writing {len(bset)} to file {idx}.txt')
 
 def create_array_bands(groupID, workdir, bands, repeat_id, dryrun=False):
+    """
+    Create band-files (under repeat_id) for this set of datasets.
+
+    :returns: None
+    """
+    # Ensure band directory exists
     bands_path = f'{workdir}/groups/{groupID}/proj_codes/{repeat_id}/'
     if not os.path.isdir(bands_path):
         if not dryrun:
             os.makedirs(bands_path)
+        else:
+            print(f'Making directories: {bands_path}')
 
     for b in bands:
         if not dryrun:
+            # Export proj codes to correct band file
             os.system(f'touch {bands_path}/band_{b}.txt')
             with open(f'{bands_path}/band_{b}.txt','w') as f:
                     f.write('\n'.join(bands[b]))
