13e5e227a0be6dfbd35a1bba8f89f8ee99e2d1c2
dwest77 <daniel.westwood@stfc.ac.uk>
2024-05-30 16:27:17 +0100

    Added CEDA/ESA logos

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-project = 'Padocc Package'
+project = 'PADOCC Package'
 copyright = '2024, Daniel Westwood'
 author = 'Daniel Westwood'
 release = 'v1.1'
