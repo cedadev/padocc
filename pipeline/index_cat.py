1f24adc01281f7863b814c5324238cccac3f422f
dwest77a <daniel.westwood@stfc.ac.uk>
2024-06-21 14:53:46 +0100

    Initial commit of catalog tool

--- pipeline/index_cat.py
+++ pipeline/index_cat.py
@@ -9,3 +9,66 @@ __copyright__ = "Copyright 2023 United Kingdom Research and Innovation"
 # but this would mean multiple updates?
 
 # Any Elasticsearch index should also include the group name (possibly part of each record?) for search purposes.
+
+from ceda_elasticsearch_tools import BulkClient
+import json
+import os
+
+from datetime import datetime
+
+phase = {
+    'total':None,
+    'complete':None,
+    'pending':None,
+    'issue':None
+}
+
+group_template = {
+    'group':'CMIP6_rel1_6233',
+    'collection': 'CMIP6',
+    'last_upload':None,
+    'total_codes':'5774',
+    'scan': phase,
+    'compute': phase,
+    'validate': phase,
+    'blacklist': 92
+}
+
+dataset_template = {
+    'DRI':None,
+    'group':'CMIP6_rel1_6233',
+    'collection':'CMIP6',
+    'last_upload':None,
+    'phase':None,
+    'status':None,
+
+    'netcdf_data':'Unknown',
+    'kerchunk_data':'Unknown',
+    'num_files':'Unknown',
+    'format':'Unknown',
+    'chunks_per_file':'Unknown'
+}
+
+def catalog_groups(group_ids,test=None):
+    # Determine values for each group
+    # Fill template dict accordingly
+    # Add records using client
+
+
+    #pad = PadoccClient('padocc-groups','group')
+
+    pad = BulkClient('padocc-groups','group',es_config='/Users/daniel.westwood/cedadev/padocc/config/es_config.json')
+    pad.add_records(test)
+    pass
+
+def catalog_products(groupID):
+    # Take group ID
+    # Catalog all products within the group
+    # Create collections within the ES index.
+    pass
+
+
+if __name__ == '__main__':
+    test = group_template
+    test['last_upload'] = datetime.strftime(datetime.now(),'%Y-%m-%d %H:%M:%S')
+    catalog_groups(None, test=[test])
