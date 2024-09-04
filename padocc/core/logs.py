4fd811df35621582d23eade59a4e5c97cf5e1208
dwest77 <daniel.westwood@stfc.ac.uk>
2024-09-04 08:51:41 +0100

    Removed old functions, updated license

--- padocc/core/logs.py
+++ padocc/core/logs.py
@@ -1,11 +1,9 @@
 __author__    = "Daniel Westwood"
 __contact__   = "daniel.westwood@stfc.ac.uk"
-__copyright__ = "Copyright 2023 United Kingdom Research and Innovation"
+__copyright__ = "Copyright 2024 United Kingdom Research and Innovation"
 
 import logging
 import os
-from padocc.errors import MissingVariableError
-from datetime import datetime
 
 levels = [
     logging.WARN,
@@ -20,42 +18,6 @@ SUFFIXES = {
     'M': 1000000,
     'G': 1000000000
 }
-
-def log_status(phase, proj_dir, status, logger, jobid='', dryrun=''):
-    """Find the status file for this project code, add a new entry for the status.
-    - Entry should be of the form:
-    - phase, status, time, jobid
-    """
-
-    # Create file if not already present
-    status_log = f'{proj_dir}/status_log.csv'
-    if not os.path.isfile(status_log):
-        logger.debug(f'Creating status file {status_log}')
-        os.system(f'touch {status_log}')
-    
-    # Open existing file
-    with open(status_log) as f:
-        lines = [r.strip() for r in f.readlines()]
-
-    # Add new content from most recent run
-    status = status.replace(',', '.').replace('\n','.')
-    lines.append(f'{phase},{status},{datetime.now().strftime("%H:%M %D")},{jobid},{dryrun}')
-
-    # Save content
-    with open(status_log, 'w') as f:
-        f.write('\n'.join(lines))
-    logger.info(f'Updated new status: {phase} - {status}')
-
-def get_log_status(proj_dir, last=True):
-    current = {}
-    status_log = f'{proj_dir}/status_log.csv'
-    if os.path.isfile(status_log):
-        with open(status_log) as f:
-            current = [r.strip() for r in f.readlines()]
-    if last:
-        return current[-1]
-    else:
-        return current
 
 class FalseLogger:
     def __init__(self):
