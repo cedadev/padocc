624ad2602d1f1a72b6d7ff805eb0b84e7bcd7dc2
dwest77 <daniel.westwood@stfc.ac.uk>
2024-09-06 11:28:13 +0100

    Added logged operation, docstrings and typehints

--- padocc/core/logs.py
+++ padocc/core/logs.py
@@ -19,19 +19,63 @@ SUFFIXES = {
     'G': 1000000000
 }
 
+class LoggedOperation:
+    """
+    Allows inherritance of logger objects without creating new ones.
+    """
+    def __init__(
+            self, 
+            logger : logging.logger,
+            label  : str = None, 
+            fh     : str = None, 
+            logid  : str = None, 
+            verbose: int = 0
+        ) -> None:
+
+        self._verbose = verbose
+        if hasattr(self, 'logger'):
+            return
+        if logger is None:
+            self.logger = init_logger(
+                self._verbose, 
+                label,
+                fh=fh, 
+                logid=logid)
+        else:
+            self.logger = logger
+
 class FalseLogger:
+    """
+    Supplementary class where a logger is not wanted but is required for
+    some operations.
+    """
     def __init__(self):
         pass
-    def debug(self, message):
+    def debug(self, message: str):
         pass
-    def info(self, message):
+    def info(self, message: str):
         pass
-    def warning(self, message):
+    def warning(self, message: str):
         pass
-    def error(self, message):
+    def error(self, message: str):
         pass
 
-def reset_file_handler(logger, verbose, new_log):
+def reset_file_handler(
+        logger  : logging.Logger,
+        verbose : int, 
+        fh : str
+    ) -> logging.logger:
+    """
+    Reset the file handler for an existing logger object.
+
+    :param logger:      (logging.logger) An existing logger object.
+
+    :param verbose:     (int) The logging level to reapply.
+
+    :param fh:     (str) Address to new file handler.
+
+    :returns:   A new logger object with a new file handler.
+    """
     logger.handlers.clear()
     verbose = min(verbose, len(levels)-1)
 
@@ -42,19 +86,52 @@ def reset_file_handler(logger, verbose, new_log):
     ch.setFormatter(formatter)
     logger.addHandler(ch)
 
-    fh = logging.FileHandler(new_log)
-    fh.setLevel(levels[verbose])
-    fh.setFormatter(formatter)
-    logger.addHandler(fh)
+    fdir = '/'.join(fh.split('/')[:-1])
+    if not os.path.isdir(fdir):
+        os.makedirs(fdir)
+    if os.path.isfile(fh):
+        os.system(f'rm {fh}')
+
+    os.system(f'touch {fh}')
+
+    fhandle = logging.FileHandler(fh)
+    fhandle.setLevel(levels[verbose])
+    fhandle.setFormatter(formatter)
+    logger.addHandler(fhandle)
 
     return logger
 
-def init_logger(verbose, name, fh=None, logid=None):
-    """Logger object init and configure with formatting"""
+def init_logger(
+        verbose : int, 
+        name  : str, 
+        fh    : str = None, 
+        logid : str = None
+    ) -> logging.logger:
+    """
+    Logger object init and configure with standardised formatting.
+    
+    :param verbose:     (int) Level of verbosity for log messages (see core.init_logger).
+
+    :param name:        (str) The label to apply to the logger object.
+
+    :param fh:          (str) Path to logfile for logger object generated in this specific process.
+
+    :param logid:       (str) ID of the process within a subset, which is then added to the name
+        of the logger - prevents multiple processes with different logfiles getting
+        loggers confused.
+
+    :returns:       A new logger object.
+    
+    """
 
     verbose = min(verbose, len(levels)-1)
-    if logid != None:
+    if logid is not None:
         name = f'{name}_{logid}'
+
+    logger = logging.getLogger(name)
+
+    if fh:
+        return reset_file_handler(logger, verbose, fh)
 
     logger = logging.getLogger(name)
     logger.setLevel(levels[verbose])
@@ -65,19 +142,5 @@ def init_logger(verbose, name, fh=None, logid=None):
     formatter = logging.Formatter('%(levelname)s [%(name)s]: %(message)s')
     ch.setFormatter(formatter)
     logger.addHandler(ch)
-
-    if fh:
-        fdir = '/'.join(fh.split('/')[:-1])
-        if not os.path.isdir(fdir):
-            os.makedirs(fdir)
-        if os.path.isfile(fh):
-            os.system(f'rm {fh}')
-
-        os.system(f'touch {fh}')
-
-        handle = logging.FileHandler(fh)
-        handle.setLevel(levels[verbose])
-        handle.setFormatter(formatter)
-        logger.addHandler(handle)
 
     return logger
