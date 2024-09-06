053190a266a95aeb40bc7fc0117010c18edec11a
dwest77 <daniel.westwood@stfc.ac.uk>
2024-09-06 12:38:31 +0100

    First version of operations

--- padocc/operations/operations.py
+++ padocc/operations/operations.py
@@ -1,43 +1,69 @@
-from padocc.logs import init_logger
-from .filehandlers import (
-    PadoccFileHandler,
+__author__    = "Daniel Westwood"
+__contact__   = "daniel.westwood@stfc.ac.uk"
+__copyright__ = "Copyright 2024 United Kingdom Research and Innovation"
+
+from padocc.core import (
+    FalseLogger,
+    LoggedOperation, 
+    BypassSwitch
+)
+
+from padocc.operations.filehandlers import (
     JSONFileHandler, 
     CSVFileHandler,
     TextFileHandler,
     LogFileHandler,
+    KerchunkFile,
 )
-from padocc.utils import extract_file
+from padocc.core.utils import extract_file
 
 import os
-import json
-
-class LoggedOperation:
-    
-    def __init__(self):
-        raise NotImplementedError
-
-    def _load_logger(self, logger, label=None, fh=None, logid=None, verbose=0):
-        self._verbose = verbose
-        if hasattr(self, 'logger'):
-            return
-        if not logger:
-            self.logger = init_logger(
-                self._verbose, 
-                label,
-                fh=fh, 
-                logid=logid)
-        else:
-            self.logger = logger
+import logging
 
 class DirectoryOperation(LoggedOperation):
-    def __init__(self, workdir, groupID=None, forceful=None, dryrun=None, logger=None, **kwargs):
+    """
+    Container class for Operations which require functionality to create
+    directories (workdir, groupdir, cache etc.)
+    """
+
+    def __init__(
+            self, 
+            workdir, 
+            groupID=None, forceful=None, dryrun=None, 
+            thorough=None, logger=None, bypass=None, label=None, 
+            fh=None, logid=None, verbose=None):
+        
         self.workdir = workdir
         self.groupID = groupID
 
+        self._thorough = thorough
+        self._bypass   = bypass
+
+        self._set_fh_kwargs(forceful=forceful, dryrun=dryrun)
+
+        super().__init__(
+            logger,
+            label=label,
+            fh=fh,
+            logid=logid,
+            verbose=verbose)
+
+    @property
+    def fh_kwargs(self):
+        return {
+            'dryrun': self._dryrun,
+            'forceful': self._forceful,
+            'verbose': self._verbose,
+        }
+    
+    @fh_kwargs.setter
+    def fh_kwargs(self, value):
+        self._set_fh_kwargs(**value)
+
+    def _set_fh_kwargs(self, forceful=None, dryrun=None, verbose=None):
         self._forceful = forceful
         self._dryrun   = dryrun
-
-        self._load_logger(logger, **kwargs)
+        self._verbose  = verbose
 
     def _setup_workdir(self):
         if not os.path.isdir(self.workdir):
@@ -58,6 +84,14 @@ class DirectoryOperation(LoggedOperation):
         self._setup_workdir()
         self._setup_groupdir()
 
+    def _setup_cache(self):
+        self.cache = f'{self.dir}/cache'
+
+        if not os.path.isdir(self.cache):
+            os.makedirs(self.cache) 
+        if self._thorough:
+            os.system(f'rm -rf {self.cache}/*')
+
     @property
     def groupdir(self):
         if self.groupID:
@@ -69,8 +103,68 @@ class DirectoryOperation(LoggedOperation):
 
 class GroupOperation(DirectoryOperation):
 
-    def __init__(self, workdir=None, **kwargs):
-        super().__init__(workdir=workdir, **kwargs)
+    def __init__(
+            self, 
+            groupID : str,
+            workdir : str = None, 
+            forceful : bool = None,
+            dryrun   : bool = None,
+            thorough : bool = None,
+            logger   : logging.logger | FalseLogger = FalseLogger(),
+            bypass   : BypassSwitch = BypassSwitch(),
+            label    : str = None,
+            fh       : str = None,
+            logid    : str = None,
+            verbose  : int = None,
+        ) -> None:
+        """
+        Initialisation for a GroupOperation object to handle all interactions
+        with all projects within a group. 
+
+        :param groupID:         (str) Name of current dataset group.
+
+        :param workdir:         (str) Path to the current working directory.
+
+        :param forceful:        (bool) Continue with processing even if final output file 
+            already exists.
+
+        :param dryrun:          (bool) If True will prevent output files being generated
+            or updated and instead will demonstrate commands that would otherwise happen.
+
+        :param thorough:        (bool) From args.quality - if True will create all files 
+            from scratch, otherwise saved refs from previous runs will be loaded.
+
+        :param logger:          (logging.logger | FalseLogger) An existing logger object.
+                                    
+        :param bypass:              (BypassSwitch) instance of BypassSwitch class containing multiple
+                                    bypass/skip options for specific events. See utils.BypassSwitch.
+
+        :param label:               (str) The label to apply to the logger object.
+
+        :param fh:                  (str) Path to logfile for logger object generated in this specific process.
+
+        :param logid:               (str) ID of the process within a subset, which is then added to the name
+                                    of the logger - prevents multiple processes with different logfiles getting
+                                    loggers confused.
+
+        :param verbose:         (int) Level of verbosity for log messages (see core.init_logger).
+
+        :returns: None
+
+        """
+
+        super().__init__(
+            workdir,
+            groupID=groupID, 
+            forceful=forceful,
+            dryrun=dryrun,
+            thorough=thorough,
+            logger=logger,
+            bypass=bypass,
+            label=label,
+            fh=fh,
+            logid=logid,
+            verbose=verbose)
 
         self.proj_codes      = {}
         self.blacklist_codes = CSVFileHandler(
@@ -93,6 +187,9 @@ class GroupOperation(DirectoryOperation):
 
     def run(self):
         raise NotImplementedError
+    
+    def add_project(self):
+        pass
 
     def save_proj_codes(self):
         for pc in self.proj_codes:
@@ -103,7 +200,7 @@ class GroupOperation(DirectoryOperation):
         self.datasets.save_file()
         self.save_proj_codes(self)
 
-    def add_proj_codeset(self, name, newcodes):
+    def add_proj_codeset(self, name : str, newcodes : list):
         self.proj_codes[name] = TextFileHandler(
             self.proj_codes_dir,
             name,
@@ -154,101 +251,105 @@ class GroupOperation(DirectoryOperation):
             if self._dryrun:
                 self.logger.debug(f'DRYRUN: Skip making codes-dir for {self.groupID}')
             os.makedirs(codes_dir)
-    
-    # Replacing the below using FileHandlers
-    def get_codes(self, repeat_id) -> list:
+            
+class ProjectOperation(DirectoryOperation):
+    """
+    PADOCC Project Operation class, able to access project files
+    and perform some simple functions. Single-project operations
+    always inherit from this class (e.g. Scan, Compute, Validate)
+    """
+
+    def __init__(
+            self, 
+            proj_code : str, 
+            workdir   : str,
+            groupID   : str = None, 
+            first_time : bool = None,
+            ft_kwargs  : dict = None,
+            logger     : logging.logger = None,
+            bypass     : BypassSwitch = BypassSwitch(),
+            label      : str = None,
+            fh         : str = None,
+            logid      : str = None,
+            verbose    : bool = None,
+            forceful   : bool = None,
+            dryrun     : bool = None,
+            thorough   : bool = None
+        ) -> None:
         """
-        Returns a list of the project codes given a filename (repeat id)
+        Initialisation for a ProjectOperation object to handle all interactions
+        with a single project. 
 
-        :param repeat_id:       (str) Identifier for a subset list of project codes
+        :param proj_code:       (str) The project code in string format (DOI)
 
-        :returns: A list of codes if the file is found, an empty list otherwise.
-        """
-        return self.proj_codes[repeat_id].get
-        
-    def set_codes(self, filename: str, contents, extension='.txt', overwrite=0) -> None:
-        """
-        Returns a list of the project codes given a filename (repeat id)
+        :param workdir:         (str) Path to the current working directory.
 
-        :param filename:    (str) Name of text file to access within group (or path
-                            within the groupdir to the text file
-        
-        :param contents:    (str) Combined contents to write to the file.
+        :param groupID:         (str) Name of current dataset group.
 
-        :param extension:   (str) For the specific case of non-text-files.
+        :param first_time:
 
-        :param overwrite:   (str) Specifier for open() built-in python method, completely
-                            overwrite the file contents or append to existing values.
+        :param ft_kwargs:
+
+        :param logger:
+                                    
+        :param bypass:              (BypassSwitch) instance of BypassSwitch class containing multiple
+                                    bypass/skip options for specific events. See utils.BypassSwitch.
+
+        :param label:               (str) The label to apply to the logger object.
+
+        :param fh:                  (str) Path to logfile for logger object generated in this specific process.
+
+        :param logid:               (str) ID of the process within a subset, which is then added to the name
+                                    of the logger - prevents multiple processes with different logfiles getting
+                                    loggers confused.
+
+        :param verbose:         (int) Level of verbosity for log messages (see core.init_logger).
+
+        :param forceful:        (bool) Continue with processing even if final output file 
+            already exists.
+
+        :param dryrun:          (bool) If True will prevent output files being generated
+            or updated and instead will demonstrate commands that would otherwise happen.
+
+        :param thorough:        (bool) From args.quality - if True will create all files 
+            from scratch, otherwise saved refs from previous runs will be loaded.
 
         :returns: None
+
         """
-        codefile = f'{self.groupdir}/{filename}{extension}'
 
-        ow = 'w'
-        if overwrite == 1:
-            ow = 'w+'
+        super().__init__(
+            workdir,
+            groupID=groupID, 
+            forceful=forceful,
+            dryrun=dryrun,
+            thorough=thorough,
+            logger=logger,
+            bypass=bypass,
+            label=label,
+            fh=fh,
+            logid=logid,
+            verbose=verbose)
 
-        with open(codefile, ow) as f:
-            f.write(contents)
-        
-    def get_blacklist(self) -> list:
-        """
-        Returns a list of the project codes given a filename (repeat id)
-
-        :param group:       (str) Name of current group or path to group directory
-                            (groupdir) in which case workdir can be left as None.
-
-        :param workdir:     (str) Path to working directory or None. If this is None,
-                            group value will be assumed as the groupdir path.
-
-        :returns: A list of codes if the file is found, an empty list otherwise.
-        """
-        codefile = f'{self.groupdir}/blacklist_codes.txt'
-        if os.path.isfile(codefile):
-            with open(codefile) as f:
-                contents = [r.strip().split(',') for r in f.readlines()]
-                if type(contents[0]) != list:
-                    contents = [contents]
-                return contents
-        else:
-            return []
-
-class ProjectOperation(DirectoryOperation):
-    def __init__(
-        self, 
-        workdir, 
-        proj_code, 
-        groupID=None, 
-        logger=None, 
-        pattern=None,
-        first_time=None, 
-        **kwargs):
-        self._proj_code = proj_code
-
-        super().__init__(workdir, groupID=groupID, logger=None)
+        self.proj_code = proj_code
 
         # Need a first-time initialisation implementation for some elements.
-
-        self._setup_dir()
+    
         self._create_dirs(first_time=first_time)
 
         # Project FileHandlers
+        self.base_cfg   = JSONFileHandler(self.dir, 'base-cfg', self.logger, **self.fh_kwargs)
+        self.detail_cfg = JSONFileHandler(self.dir, 'detail-cfg', self.logger, **self.fh_kwargs)
+        self.allfiles   = TextFileHandler(self.dir, 'allfiles', self.logger, **self.fh_kwargs)
 
-        fh_kwargs = {
-            'dryrun':self._dryrun,
-            'forceful':self._forceful,
-        }
-
-        # ProjectOperation properties
-        self.base_cfg   = JSONFileHandler(self.dir, 'base-cfg', self.logger, **fh_kwargs)
-        self.detail_cfg = JSONFileHandler(self.dir, 'detail-cfg', self.logger, **fh_kwargs)
-        self.allfiles   = TextFileHandler(self.dir, 'allfiles', self.logger, **fh_kwargs)
-
-        if pattern and first_time:
-            self.configure_filelist(pattern)
+        # ft_kwargs <- stored in base_cfg after this point.
+        if first_time:
+            if isinstance(ft_kwargs, dict):
+                self._setup_config(**ft_kwargs)
+            self.configure_filelist()
 
         # ProjectOperation attributes
-        self.status_log = CSVFileHandler(self.dir, 'status_log', self.logger, **fh_kwargs)
+        self.status_log = CSVFileHandler(self.dir, 'status_log', self.logger, **self.fh_kwargs)
 
         self.phase_logs = {}
         for phase in ['scan', 'compute', 'validate']:
@@ -257,17 +358,40 @@ class ProjectOperation(DirectoryOperation):
                 phase, 
                 self.logger, 
                 extra_path='phase_logs/', 
-                **fh_kwargs
+                **self.fh_kwargs
             )
-        if first_time:
-            self._setup_config(**kwargs)
-            if pattern:
-                self._configure_filelist(pattern)
+
+        self.kfile  = None
+        self.kstore = None
+        self.zstore = None
+
+    def create_new_kfile(self, product : str):
+        self.kfile = KerchunkFile(
+            self.dir,
+            product,
+            self.logger,
+            **self.fh_kwargs
+        )
+
+    def create_new_kstore(self, product : str):
+        raise NotImplementedError
+
+    @property
+    def outfile(self):
+        if self._outfile:
+            return self._outfile
+        
+        # Assemble the outfile
+        return None
+    
+    @outfile.setter
+    def outfile(self, value : str):
+        self._outfile = value
 
     def __str__(self):
-        return self._proj_code
+        return self.proj_code
 
-    def dir_exists(self, checkdir=None):
+    def dir_exists(self, checkdir : str = None):
         if not checkdir:
             checkdir = self.dir
 
@@ -275,28 +399,42 @@ class ProjectOperation(DirectoryOperation):
             return True
         return False
 
-    def file_exists(self, file):
+    def file_exists(self, file : str):
+        """Check if a named file exists (without extension)"""
         if hasattr(self, file):
             fhandle = getattr(self, file)
         return fhandle.file_exists()
 
-    def set_last_run(self, phase, time) -> None:
+    def set_last_run(self, phase: str, time : str) -> None:
         lr = (phase, time)
         self.detail_cfg.set(lr, 'last_run')
 
     def get_last_run(self) -> tuple:
         return self.detail_cfg.get('last_run')
 
-    def update_status(self, phase, status, jobid='', dryrun=''):
+    def update_status(
+            self, 
+            phase : str, 
+            status: str, 
+            jobid : str = '', 
+            dryrun: str = ''
+        ) -> None: 
         self.status_log.update_status(phase, status, jobid=jobid, dryrun=dryrun)
 
     def save_files(self):
         # Add all files here.
-        self.base_cfg._set_content()
-        self.detail_cfg._set_content()
-        self.allfiles._set_content()
+        self.base_cfg.save_file()
+        self.detail_cfg.save_file()
+        self.allfiles.save_file()
 
-    def _configure_filelist(self, pattern):
+    def _configure_filelist(self):
+        pattern = self.base_cfg.get('pattern')
+
+        if not pattern:
+            raise ValueError(
+                '"pattern" attribute missing from base config.'
+            )
+        
         if not pattern.endswith('.txt'):
             # Expand pattern
             raise NotImplementedError
@@ -306,42 +444,15 @@ class ProjectOperation(DirectoryOperation):
         else:
             self.allfiles = extract_file(pattern)
 
-    # Base cfg
-    @property
-    def base_cfg(self):
-        return self._base_cfg
-
-    @base_cfg.setter
-    def base_cfg(self, obj):
-        if isinstance(obj, PadoccFileHandler):
-            self._base_cfg = obj
-        else:
-            print(type(obj))
-            self._base_cfg.set(obj)
-
-    @property
-    def detail_cfg(self):
-        return self._detail_cfg
-
-    @detail_cfg.setter
-    def detail_cfg(self, obj):
-        if isinstance(obj, PadoccFileHandler):
-            self._detail_cfg = obj
-        else:
-            self._detail_cfg.set(obj)
-
-    @property
-    def allfiles(self):
-        return self._allfiles
-
-    @allfiles.setter
-    def allfiles(self, obj):
-        if isinstance(obj, PadoccFileHandler):
-            self._allfiles = obj
-        else:
-            self._allfiles.set(obj)
-
-    def _setup_config(self, pattern=None, update=None, remove=None):
+    def _setup_config(
+            self, 
+            pattern : str = None, 
+            update : str = None, 
+            remove : str = None
+        ) -> None:
+        """
+        Create base cfg json file with all required parameters.
+        """
         if pattern or update or remove:
             config = {
                 'proj_code':self.proj_code,
@@ -349,15 +460,16 @@ class ProjectOperation(DirectoryOperation):
                 'updates':update,
                 'removals':remove,
             }
-            self.base_cfg = config
+            self.base_cfg.set(config)
 
-    def _setup_dir(self):
+    @property
+    def dir(self):
         if self.groupID:
-            self.dir = f'{self.workdir}/in_progress/{self.groupID}/{self._proj_code}'
+            return f'{self.workdir}/in_progress/{self.groupID}/{self.proj_code}'
         else:
             raise NotImplementedError
-        
-    def _create_dirs(self, first_time=None):
+
+    def _create_dirs(self, first_time : bool = None):
         if not self.dir_exists():
             if self._dryrun:
                 self.logger.debug(f'DRYRUN: Skip making project directory for: "{self}"')
@@ -375,7 +487,7 @@ class ProjectOperation(DirectoryOperation):
             if first_time:
                 self.logger.warning(f'"{logdir}" already exists.')
 
-    def _get_log(self, phase):
+    def _get_log(self, phase: str):
         """
         Fetch a log file from a previous run for this 
         dataset for a particular phase.
@@ -389,14 +501,14 @@ class ProjectOperation(DirectoryOperation):
         )
         return '!No log data!'
 
-    def _examine_log(self, phase, error=None, rerun=None, halt=False):
+    def _examine_log(self, phase : str, error=None, rerun=None, halt=False):
         """
         Examine the log file for this project given a specific phase, possibly looking for a 
         specific error.
         """
         print(self._get_log(phase))
 
-        self.logger.info(f'Project Code: {proj_code}')
+        self.logger.info(f'Project Code: {self.proj_code}')
         if error:
             self.logger.info(f'Error: {error}')
         if rerun:
