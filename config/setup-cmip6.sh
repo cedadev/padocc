0af0290be588f29b564681cfcfe680784375addb
dwest77 <daniel.westwood@stfc.ac.uk>
2024-09-06 14:46:46 +0100

    Moved content to padocc_cli

--- config/setup-cmip6.sh
+++
@@ -1,8 +0,0 @@
-module load jaspy
-
-export WORKDIR=/gws/nopw/j04/cmip6_prep_vol1/kerchunk-pipeline
-export GROUPDIR=/gws/nopw/j04/cmip6_prep_vol1/kerchunk-pipeline/groups/CMIP6_rel1_6233
-export SRCDIR=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder
-export KVENV=/home/users/dwest77/Documents/kerchunk_dev/kerchunk-builder/build_venv
-
-source $KVENV/bin/activate
