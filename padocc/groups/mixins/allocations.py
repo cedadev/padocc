4e009fed220ba197e051d03e52ed48ca72587f5c
dwest77a <daniel.westwood@stfc.ac.uk>
2025-03-06 08:50:59 +0000

    Added parallel flag, fixed lotus cfg retrieval

--- padocc/groups/mixins/allocations.py
+++ padocc/groups/mixins/allocations.py
@@ -3,15 +3,42 @@ __contact__   = "daniel.westwood@stfc.ac.uk"
 __copyright__ = "Copyright 2024 United Kingdom Research and Innovation"
 
 import os
-from typing import Callable
+import json
+from typing import Callable, Union
 
 import binpacking
 
 from padocc import ProjectOperation
 from padocc.core.filehandlers import ListFileHandler
-from padocc.core.utils import BypassSwitch, times
-from padocc.phases import KNOWN_PHASES
+from padocc.core.utils import BypassSwitch, times, parallel_modes
 
+def get_lotus_reqs(logger):
+    """
+    Extract Lotus config from filesystem.
+    
+    Use default if no config supplied.
+    """
+    refs = {}
+    cfg  = os.environ.get('LOTUS_CFG',None)
+
+    if cfg is not None:
+        if os.path.isfile(cfg):
+            with open(cfg) as f:
+                refs = json.load(f)
+
+    if refs.get('lotus_vn',None) == 2:
+        logger.info("Using Lotus2 Deployment Specs.")
+        return [
+            f'#SBATCH --partition={refs.get("partition","standard")}',
+            f'#SBATCH --account={refs.get("account","no-project")}',
+            f'#SBATCH --qos={refs.get("qos","standard")}'
+        ]
+
+    else:
+        logger.info("Using Lotus Legacy Deployment Specs.")
+        return [
+            f'#SBATCH --partition={refs.get("partition","short-serial")}'
+        ]
 
 class AllocationsMixin:
     """
@@ -32,26 +59,30 @@ class AllocationsMixin:
     def help(cls, func: Callable = print):
         func('Allocations:')
         func(' > group.create_allocations() - Create a set of allocations, returns a binned list of bands')
-        func(' > group.create_sbatch() - Create sbatch script for submitting to slurm.')
+        func(' > group.deploy_parallel() - Create sbatch script for submitting to slurm.')
 
     def create_allocations(
             self,
-            phase,
-            repeat_id,
-            band_increase=None,
-            binpack=None,
+            phase       : str,
+            repeat_id   : str,
+            band_increase : Union[str,None] = None,
+            binpack     : bool = None,
             **kwargs,
         ) -> list:
         """
-        Function for assembling all allocations and bands for packing. Allocations contain multiple processes within
-        a single SLURM job such that the estimates for time sum to less than the time allowed for that SLURM job. Bands
-        are single process per job, based on a default time plus any previous attempts (use --allow-band-increase flag
-        to enable band increases with successive attempts if previous jobs timed out)
+        Function for assembling all allocations and bands for packing. 
+        
+        Allocations contain multiple processes within a single SLURM job such 
+        that the estimates for time sum to less than the time allowed for that SLURM job. 
+        Bands are single process per job, based on a default time plus any previous 
+        attempts (use --allow-band-increase flag to enable band increases with successive 
+        attempts if previous jobs timed out)
 
-        :returns:   A list of tuple objects such that each tuple represents an array to submit to slurm with
-                    the attributes (label, time, number_of_datasets). Note: The list of datasets to apply in
-                    each array job is typcially saved under proj_codes/<repeat_id>/<label>.txt (allocations use
-                    allocations/<x>.txt in place of the label)
+        :returns:   A list of tuple objects such that each tuple represents an array 
+            to submit to slurm with the attributes (label, time, number_of_datasets). 
+            Note: The list of datasets to apply in each array job is typcially saved 
+            under proj_codes/<repeat_id>/<label>.txt (allocations use allocations/<x>.txt 
+            in place of the label)
         """
 
         proj_codes = self.proj_codes[repeat_id]
@@ -81,12 +112,6 @@ class AllocationsMixin:
                 else:
                     # Use default if no prior info found.
                     next_band = time_defs_value
-
-                # Thorough/Quality validation - special case.
-                #if 'quality_required' in detail and phase == 'validate':
-                    #if detail['quality_required']:
-                        # Hardcoded quality time 2 hours
-                        #next_band = max(next_band, 120) # Min 2 hours
 
                 # Save code to specific band
                 if next_band in time_bands:
@@ -120,12 +145,12 @@ class AllocationsMixin:
         allocs = []
         # Create allocations
         if bins:
-            _create_allocations(self.groupID, self.workdir, bins, repeat_id, dryrun=self.dryrun)
+            self._create_allocations(bins, repeat_id)
             if len(bins) > 0:
                 allocs.append(('allocations','240:00',len(bins)))
 
         # Create array bands
-        _create_array_bands(self.groupID, self.workdir, time_bands, repeat_id, dryrun=self.dryrun)
+        self._create_array_bands(time_bands, repeat_id)
             
         if len(time_bands) > 0:
             for b in time_bands:
@@ -134,37 +159,39 @@ class AllocationsMixin:
         # Return list of tuples.
         return allocs
     
-    def create_sbatch(
+    def deploy_parallel(
             self,
-            phase     : str,
-            source    : str = None,
-            venvpath  : str = None,
-            band_increase : str = None,
-            forceful   : bool = None,
-            dryrun     : bool = None,
-            quality    : bool = None,
-            verbose    : int = 0,
-            binpack    : bool = None,
-            time_allowed : str = None,
-            memory       : str = None,
-            subset       : int = None,
-            repeat_id    : str = 'main',
-            bypass       : BypassSwitch = BypassSwitch(),
-            mode         : str = 'kerchunk',
-            new_version  : str = None,
+            phase           : str,
+            source          : str = None,
+            band_increase   : str = None,
+            forceful        : bool = None,
+            dryrun          : bool = None,
+            thorough        : bool = None,
+            verbose         : int = 0,
+            binpack         : bool = None,
+            time_allowed    : str = None,
+            memory          : str = None,
+            subset          : int = None,
+            repeat_id       : str = 'main',
+            bypass          : BypassSwitch = BypassSwitch(),
+            mode            : str = 'kerchunk',
+            new_version     : str = None,
+            func            : Callable = print,
         ) -> None:
+        """
+        Organise parallel deployment via SLURM.
+        """
 
-        if phase not in KNOWN_PHASES:
+        if phase not in parallel_modes:
             raise ValueError(
-                f'"{phase}" not recognised, please select from {KNOWN_PHASES}'
+                f'"{phase}" not recognised, please select from {parallel_modes}'
             )
-            return None
 
-        array_job_kwargs = {
-            'forceful': forceful,
-            'dryrun'  : dryrun,
-            'quality' : quality,
-            'verbose' : verbose,
+        sbatch_kwargs = {
+            'forceful': forceful or self._forceful,
+            'dryrun'  : dryrun or self._dryrun,
+            'thorough' : thorough or self._thorough,
+            'verbose' : verbose or self._verbose,
             'binpack' : binpack,
             'time_allowed' : time_allowed,
             'memory'  : memory,
@@ -175,6 +202,10 @@ class AllocationsMixin:
             'new_version' : new_version,
         }
 
+        # Ensure directories are created for logs
+        self._setup_slurm_directories()
+        sbatch_dir  = f'{self.dir}/sbatch/'
+
         # Perform allocation assignments here.
         if not time_allowed:
             allocations = self.create_allocations(
@@ -183,18 +214,43 @@ class AllocationsMixin:
             )
 
             for alloc in allocations:
-                print(f'{alloc[0]}: ({alloc[1]}) - {alloc[2]} Jobs')
+                func(f'{alloc[0]}: ({alloc[1]}) - {alloc[2]} Jobs')
 
             deploy = input('Deploy the above allocated dataset jobs with these timings? (Y/N) ')
             if deploy != 'Y':
                 raise KeyboardInterrupt
 
-            for alloc in allocations:
-                self._create_job_array(
-                    phase, source, venvpath, alloc[2]
-                    **array_job_kwargs,
+            for aid, alloc in enumerate(allocations):
+
+                sbatch = ListFileHandler(
+                    sbatch_dir, 
+                    f'{phase}_{repeat_id}_{aid}.sbatch', 
+                    self.logger, 
+                    dryrun=self._dryrun, 
+                    forceful=self._forceful)
+                
+                jobname = f'PADOCC_{self.groupID}-{repeat_id}_{aid}_{phase}'
+
+                self._create_slurm_script(
+                    phase, 
+                    source, 
+                    jobname,
+                    sbatch, 
+                    group_length=alloc[2],
+                    sbatch_kwargs=sbatch_kwargs,
+                    time=alloc[1]
                 )
         else:
+
+            sbatch = ListFileHandler(
+                sbatch_dir, 
+                f'{phase}_{repeat_id}.sbatch', 
+                self.logger, 
+                dryrun=self._dryrun, 
+                forceful=self._forceful)
+            
+            jobname = f'PADOCC_{self.groupID}-{repeat_id}_{phase}'
+
             num_datasets = len(self.proj_codes[repeat_id].get())
             self.logger.info(f'All Datasets: {time_allowed} ({num_datasets})')
 
@@ -203,87 +259,61 @@ class AllocationsMixin:
             if deploy != 'Y':
                 raise KeyboardInterrupt
 
-            self._create_job_array(
-                    phase, source, venvpath, num_datasets,
-                    **array_job_kwargs,
+            self._create_slurm_script(
+                    phase, 
+                    source, 
+                    jobname,
+                    sbatch,
+                    group_length=num_datasets,
+                    sbatch_kwargs=sbatch_kwargs,
+                    time=time_allowed,
+                    memory=memory
                 )
-
-    def _create_job_array(
+            
+    def _create_slurm_script(
             self,
-            phase,
-            source,
-            venvpath,
-            group_length=None,
-            repeat_id='main',
-            forceful=None,
-            verbose=None,
-            dryrun=None,
-            quality=None,
-            bypass=None,
-            binpack=None,
-            time_allowed=None,
-            memory=None,
-            subset=None,
-            mode=None,
-            new_version=None,
-            time=None,
-            joblabel=None,
+            phase: str,
+            source: str,
+            jobname: str,
+            sbatch: ListFileHandler,
+            group_length: int,
+            sbatch_kwargs: dict,
+            time: Union[str,None] = None,
+            memory: Union[str,None] = None
         ):
-
-        sbatch_dir = f'{self.dir}/sbatch/'
-        if not joblabel:
-            sbatch_file = f'{phase}.sbatch'
-        else:
-            sbatch_file = f'{phase}_{joblabel}.sbatch'
-            repeat_id = f'{repeat_id}/{joblabel}'
-
-        sbatch = ListFileHandler(sbatch_dir, sbatch_file, self.logger, dryrun=self._dryrun, forceful=self._forceful)
-
-        master_script = f'{source}/single_run.py'
-
-        if time is None:
-            time = time_allowed or times[phase]
-        mem = '2G' or memory
-
-        jobname = f'PADOCC_{self.groupID}_{phase}'
-        if joblabel:
-            jobname = f'PADOCC_{joblabel}_{phase}_{self.groupID}'
-
-        outfile = f'{self.dir}/outs/{jobname}_{repeat_id}'
-        errfile = f'{self.dir}/errs/{jobname}_{repeat_id}'
-
-        sbatch_kwargs = self._sbatch_kwargs(
-            time,
-            memory,
-            repeat_id,
-            bypass=bypass, 
-            forceful= forceful or self._forceful, 
-            verbose = verbose or self._verbose,
-            quality = quality or self._quality, # Check
-            dryrun = dryrun or self._dryrun,
-            binpack = binpack,
-            subset = subset,
-            new_version = new_version,
-            mode = mode,
-        )
+        """
+        Create the sbatch content job array.
         
+        This content is saved to the sbatch file
+        for submission to SLURM.
+        """
+
+        time   = time or times[phase]
+        memory = memory or '2G'
+
+        outfile = f'{self.dir}/outs/{jobname}'
+        errfile = f'{self.dir}/errs/{jobname}'
+
+        sbatch_flags = self._sbatch_kwargs(time, memory, sbatch_kwargs)
+
+        lotus_requirements = get_lotus_reqs(self.logger)
+  
         sbatch_contents = [
             '#!/bin/bash',
-            '#SBATCH --partition=short-serial',
+            *lotus_requirements,
             f'#SBATCH --job-name={jobname}',
 
             f'#SBATCH --time={time}',
-            f'#SBATCH --mem={mem}',
+            f'#SBATCH --mem={memory}',
 
             f'#SBATCH -o {outfile}',
             f'#SBATCH -e {errfile}',
 
-            f'module add jaspy',
-            f'source {venvpath}/bin/activate',
+            f'source {source}/bin/activate',
 
             f'export WORKDIR={self.workdir}',
 
-            f'python {master_script} {phase} $SLURM_ARRAY_TASK_ID {sbatch_kwargs}',
+            f'padocc {phase} $SLURM_ARRAY_TASK_ID {sbatch_flags}',
         ]
 
         sbatch.update(sbatch_contents)
@@ -291,16 +321,31 @@ class AllocationsMixin:
 
         if self._dryrun:
             self.logger.info('DRYRUN: sbatch command: ')
-            print(f'sbatch --array=0-{group_length-1} {sbatch.filepath()}')
+            print(f'sbatch --array=0-{group_length-1} {sbatch.filepath}')
+        else:
+            os.system(f'sbatch --array=0-{group_length-1} {sbatch.filepath}')
 
     def _sbatch_kwargs(
-            self, time, memory, repeat_id, verbose=None, bypass=None, 
-            subset=None, new_version=None, mode=None, **bool_kwargs):
+            self, 
+            time        : str, 
+            memory      : str, 
+            repeat_id   : str, 
+            verbose     : bool = None, 
+            bypass      : Union[BypassSwitch,None] = None, 
+            subset      : Union[int,None] = None, 
+            new_version : bool = None, 
+            mode        : Union[str,None] = None, 
+            **bool_kwargs
+        ) -> str:
+        """
+        Assemble all flags and options for CLI via SLURM.
+        """
+
         sbatch_kwargs = f'-G {self.groupID} -t {time} -M {memory} -r {repeat_id}'
 
         bool_options = {
             'forceful' : '-f',
-            'quality'  : '-Q',
+            'thorough' : '-T',
             'dryrun'   : '-d',
             'binpack'  : '-A',
         }
@@ -308,7 +353,7 @@ class AllocationsMixin:
         value_options = {
             'bypass' : ('-b',bypass),
             'subset' : ('-s',subset),
-            'mode'   : ('-m',mode),
+            'mode'   : ('-C',mode),
             'new_version': ('-n',new_version),
         }
 
@@ -334,17 +379,21 @@ class AllocationsMixin:
 
     def _setup_slurm_directories(self):
         """
-        Currently Unused function to set up 
-        the slurm directories for a group."""
+        Create logging directories for this group.
+        """
 
-        for dirx in ['sbatch','errs']:
+        for dirx in ['sbatch','errs','outs']:
             if not os.path.isdir(f'{self.groupdir}/{dirx}'):
                 if self._dryrun:
                     self.logger.debug(f"DRYRUN: Skipped creating {dirx}")
                     continue
                 os.makedirs(f'{self.dir}/{dirx}')
 
-def _create_allocations(groupID: str, workdir: str, bins: list, repeat_id: str, dryrun=False) -> None:
+    def _create_allocations(
+            self, 
+            bins: list, 
+            repeat_id: str
+        ) -> None:
         """
         Create allocation files (N project codes to each file) for later job runs.
 
@@ -352,16 +401,16 @@ def _create_allocations(groupID: str, workdir: str, bins: list, repeat_id: str, 
         """
 
         # Ensure directory already exists.
-        allocation_path = f'{workdir}/groups/{groupID}/proj_codes/{repeat_id}/allocations'
+        allocation_path = f'{self.groupdir}/proj_codes/{repeat_id}/allocations'
         if not os.path.isdir(allocation_path):
-            if not dryrun:
+            if not self._dryrun:
                 os.makedirs(allocation_path)
             else:
                 print(f'Making directories: {allocation_path}')
 
         for idx, b in enumerate(bins):
             bset = b.keys()
-            if not dryrun:
+            if not self._dryrun:
                 # Create a file for each allocation
                 os.system(f'touch {allocation_path}/{idx}.txt')
                 with open(f'{allocation_path}/{idx}.txt','w') as f:
@@ -369,22 +418,26 @@ def _create_allocations(groupID: str, workdir: str, bins: list, repeat_id: str, 
             else:
                 print(f'Writing {len(bset)} to file {idx}.txt')
 
-def _create_array_bands(groupID, workdir, bands, repeat_id, dryrun=False):
+    def _create_array_bands(
+            self, 
+            bands: list, 
+            repeat_id: str
+        ) -> None:
         """
         Create band-files (under repeat_id) for this set of datasets.
 
         :returns: None
         """
         # Ensure band directory exists
-        bands_path = f'{workdir}/groups/{groupID}/proj_codes/{repeat_id}/'
+        bands_path = f'{self.groupdir}/proj_codes/{repeat_id}/'
         if not os.path.isdir(bands_path):
-            if not dryrun:
+            if not self._dryrun:
                 os.makedirs(bands_path)
             else:
                 print(f'Making directories: {bands_path}')
 
         for b in bands:
-            if not dryrun:
+            if not self._dryrun:
                 # Export proj codes to correct band file
                 os.system(f'touch {bands_path}/band_{b}.txt')
                 with open(f'{bands_path}/band_{b}.txt','w') as f:
