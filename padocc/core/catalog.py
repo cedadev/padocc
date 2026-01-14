780be4026c6734d221f77dffd0c90f2b13cdb664
dwest77 <daniel.westwood@stfc.ac.uk>
2026-01-14 17:05:10 +0000

    Initial functionality for pushing asset links to STAC

--- padocc/core/catalog.py
+++ padocc/core/catalog.py
@@ -5,47 +5,77 @@ __copyright__ = "Copyright 2024 United Kingdom Research and Innovation"
 
 import hashlib
 from typing import Union
-from elasticsearch import Elasticsearch
+import requests
 
-def catalog_ceda(
-        location: str, 
-        name: str,
-        api_key: str, 
-        cloud_format: str,
-        collection: str,
-        remote: bool,
-        version: str,
-        index: str = 'ceda-cloud-products',
-        hosts: Union[list,None] = None
-    ):
-    """
-    Catalog the output product of this project.
-    """
+REPORT_ASSET = {
+    "href": "https://dap.ceda.ac.uk",
+    "type": "application/json",
+    "title": "PADOCC Aggregation Data Report",
+    "roles": ["reference"]
+}
 
-    hosts = hosts or ['https://elasticsearch.ceda.ac.uk']
+KERCHUNK_ASSET = {
+    "open_zarr_kwargs": {
+        "decode_times": True
+      },
+      "roles": [
+        "reference",
+        "data"
+      ],
+      "href": None,
+      "type": "application/json"
+}
 
-    full_location = f'{location}/{name}'
-    id = hashlib.sha1(full_location.encode(errors="ignore")).hexdigest()
+import json
 
-    simple_json = {
-        'id': id,
-        'filename': name,
-        'directory': full_location,
-        'cloud_format': cloud_format,
-        'collection': collection,
-        'remote':remote,
-        'version':version,
-    }
+def open_json(file):
+        with open(file) as f:
+            return json.load(f)
 
-    # Push ceda record
-    es_client = Elasticsearch(
-        hosts=hosts,
-        headers={'x-api-key':api_key}
+try:
+    from httpx_auth import OAuth2ClientCredentials
+    import httpx
+
+    creds = open_json('AUTH_CREDENTIALS')
+
+    auth = OAuth2ClientCredentials(
+        "https://accounts.ceda.ac.uk/realms/ceda/protocol/openid-connect/token",
+        client_id=creds["id"],
+        client_secret=creds["secret"]
     )
 
-    es_client.update(
-        index=index,
-        id=id,
-        body={'doc': simple_json, 'doc_as_upsert': True}
+    client = httpx.Client(
+        verify=False,
+        timeout=180,
     )
-    return True
+except:
+    # Disabled auth for pushing to STAC API
+    auth = None
+    client = None
+
+def update_stac(stac_api, collection, stac_record, item_id):
+    print(f'{stac_api}/collections/{collection}/items/{item_id}', end=' > ')
+    print(client.put(
+        f'{stac_api}/collections/{collection}/items/{item_id}',
+        json=stac_record,
+        auth=auth
+    ))
+
+def add_report_asset(stac_record, report_path):
+
+    new_asset = dict(REPORT_ASSET)
+    new_asset['href'] = "https://dap.ceda.ac.uk" + report_path
+
+    stac_record['assets']['padocc_report'] = new_asset
+    return stac_record
+
+def add_kerchunk_asset(stac_record, kerchunk_path):
+    new_asset = dict(KERCHUNK_ASSET)
+    new_asset['href'] = "https://dap.ceda.ac.uk" + kerchunk_path
+
+    stac_record['assets']['reference_file'] = new_asset
+    stac_record['properties']['aggregation_status'] = 'OK'
+    return stac_record
+
+def get_stac(file_id, stac_api, collection):
+    return requests.get(f'{stac_api}/collections/{collection}/items/{file_id}').json()
